e42a1186fe1960a8c59d037e3a24c8a9a463f63e
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-09 22:32:52 +0530

    Added Carousel component and Whatsapp Connect feature in Body

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Body.css";
+import Carousel from "../Carousel/Carousel";
 
 const Body = () => {
+  const imageUrls = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEg3PGPsTqpk9MJkYziqwjA8S-leiQqgoLlZwpcyMKlEI3P84P5gQGDo4Qsn17V7dZP0I&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU3pbtiwVxGM2cnTeQ3lGvbOK3T-keMoTHqgJfgdfv1fz40PEJ5rVdWzKXm-o-voLdmlo&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTwTzE6T4FLivXc_dveIjT5mg_XV-N-3Vxypb1-lbya44ZSlsoq9bv6Ww7CFoNH-tsZTY&usqp=CAU",
+  ];
   return (
     <div className="body-container">
       <div className="left-column">
@@ -37,7 +43,20 @@ const Body = () => {
         />
       </div>
       <div className="right-column">
-        
+        <div className="carousel">
+          <Carousel imageUrls={imageUrls} />
+        </div>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="whatsapp-logo"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2044px-WhatsApp.svg.png"
+            alt="WhatsApp"
+          />
+        </a>
       </div>
     </div>
   );
