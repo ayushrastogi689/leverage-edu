1d63f50f53762e363dca5070e8084ef1dd20a2f6
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-09 15:29:01 +0530

    Feature - Created a basic Header Layout

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+import Navbar from "../Navbar/Navbar";
+const Header = () => {
+  return (
+    <header>
+      <Banner />
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
