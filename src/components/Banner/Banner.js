94023e98d2e6867c0a963970014999b681023b30
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-09 15:11:27 +0530

    Created Banner.js

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner-content">
+        <h2>Leverage Edu Scholarship worth ₹ 7,00,00,000 </h2>
+        <button>Apply Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
