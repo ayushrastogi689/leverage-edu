1d1c7650bbb314ce32db7d9a7d196f6a6a75f8e7
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-09 16:16:04 +0530

    Added Header and Body to our page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Header from "../components/Header/Header";
+import Body from "../components/Body/Body";
 
 function Home() {
   return (
     <main>
       <>
+        <Header />
+        <Body />
       </>
     </main>
   );
