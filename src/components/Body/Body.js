65f6eddf1ba40d4b54ef7fa4e958c26a6bfa09be
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-09 16:01:44 +0530

    Created Hero Section in Body Component

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,8 +1,27 @@
 import React from "react";
+import "./Body.css";
 
 const Body = () => {
   return (
-    <div >Body</div>
+    <div>
+      <div className="body-container">
+      <div className="left-column">
+        <h2>First in the Family</h2>
+        <p>
+          They are the global citizens of tomorrow. Start your abroad journey
+          today.
+        </p>
+        <div className="live-section">
+          <span> Live on </span>
+          <img
+            className="disney_hotstar"
+            src="https://i.imgur.com/U4Op2KW.jpeg"
+            alt="Disney+Hotstar"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
   );
 };
 
