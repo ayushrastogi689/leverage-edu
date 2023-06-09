d841a9522ca2d53ec82f618979bed00899a34bce
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-09 16:08:04 +0530

    Added primary & secondary button and text description

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,8 +3,7 @@ import "./Body.css";
 
 const Body = () => {
   return (
-    <div>
-      <div className="body-container">
+    <div className="body-container">
       <div className="left-column">
         <h2>First in the Family</h2>
         <p>
@@ -19,9 +18,23 @@ const Body = () => {
             alt="Disney+Hotstar"
           />
         </div>
+        <div className="buttons">
+          <button className="primary-button">
+            Get Your Shortlist for Free
+          </button>
+        </div>
+        <div className="buttons">
+          <button className="secondary-button">Talk to an expert</button>
+        </div>
+        <div className="description">
+          <p>95% of our students get an admit in less than 4 weeks</p>
+          <p>Our students finished university to work at global offices of:</p>
+        </div>
+      </div>
+      <div className="right-column">
+        
       </div>
     </div>
-  </div>
   );
 };
 
