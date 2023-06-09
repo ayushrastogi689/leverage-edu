1cd384348e088848754720f7edd76dd6c3326476
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-09 15:36:00 +0530

    Added company logo and created Navbar Links

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,9 +1,38 @@
 import React from "react";
-
-
+import "./Navbar.css";
 const Navbar = () => {
   return (
-    <div> Navbar </div> 
+    <div className="navbar">
+      <img
+        className="navbar-logo"
+        src="https://leverageedunew.s3.us-east-1.amazonaws.com/assets/img/logo.png"
+        alt="company_logo"
+      />
+
+      <ul className="navbar-links">
+        <li>
+          <a href="#">Study Abroad</a>
+        </li>
+        <li>
+          <a href="#">Accommodation</a>
+        </li>
+        <li>
+          <a href="#">Test Prep</a>
+        </li>
+        <li>
+          <a href="#">Finance</a>
+        </li>
+        <li>
+          <a href="#">Community</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">More</a>
+        </li>
+      </ul>
+</div>
   );
 };
 
